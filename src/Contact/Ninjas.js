e9a9232afb16aa4e4b8cfa80e585834e52285c97
Barnabas <Barnabas-chimaobi>
2020-03-18 23:47:50 +0100

    First commit

---
+++ src/Contact/Ninjas.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Ninjas.css'
+
+
+
+ const Ninjas = ({ninjas, deleteNinja}) => {   
+ const ninjaList = ninjas.map(ninja => {
+                if (ninja.age > 20){ 
+             return(
+          <div className='ninja' key={ninja.id}>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+                <button onClick={() => {deleteNinja(ninja.id)}}>delete ninja</button>
+          </div> 
+             )
+           }else {  
+                return null;  
+           }
+           })
+          return(
+              <div className='ninja-List'>
+              {/* {
+                   ninjas.map(ninja => {
+                    return ninja.age > 20 ? ( <div className='ninja' key={ninja.id}>
+                                  <div>Name: { ninja.name }</div>
+                                  <div>Age: { ninja.age }</div>
+                                   <div>Belt: { ninja.belt }</div>
+                               </div>) : null
+                    })
+              } */}
+              { ninjaList }
+              </div> 
+          )
+     }
+export default Ninjas;
